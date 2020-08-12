2c423f522e6fd5ae6818ffcfa272e8a77c843ea3
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-08-12 21:55:19 +0530

    Create points_distance_and_slope.cpp

---
+++ points_distance_and_slope.cpp
@@ -0,0 +1,9 @@
+class coor():
+    def  __init__(self,coor1,coor2):
+        self.coor1 = coor1
+        self.coor2 = coor2
+    def distance(self):
+        x1,x2 = self.coor1
+        return 
+    def slope(self):
+        return ((self.coor2[1]-self.coor1[1])/(self.coor2[0]-self.coor1[0]))
