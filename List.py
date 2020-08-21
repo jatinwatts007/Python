9040021646dff9531f99a55268353a679c37b1bc
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-08-21 22:06:48 +0530

    Update List.py

--- List.py
+++ List.py
@@ -1,3 +1,14 @@
 List =[i,3,5,r,g,11]
 for item in List:
     Print(item)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
