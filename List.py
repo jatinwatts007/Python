1d90e995cebdd2a149f1b1050cb2196fdc70d34a
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2021-12-31 23:21:50 +0530

    Update List.py

--- List.py
+++ List.py
@@ -11,4 +11,4 @@ thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[-4:-1])
 thislist = ["apple", "banana", "cherry"]
 if "apple" in thislist:
-  print("Yes, 'apple' is in the fruits list")
+  print("Yes, 'apple' is in the fruits lists")
